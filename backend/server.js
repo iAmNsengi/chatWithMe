219ffe339477f1735a6919ae4ceac6a229784e6f
Eliezer Nsengi <avaranstudios@gmail.com>
2024-08-15 11:30:25 +0200

    feat: add authroutes

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import authRoutes from './routes/auth.routes.js'
 
 dotenv.config();
 
@@ -9,16 +10,7 @@ const PORT = process.env.PORT || 5000;
 app.get("/", (req, res) => {
   res.send("Hello from Nsengi!");
 });
-app.get("/api/auth/signup", (req, res) => {
-  console.log("Signup route");
-});
 
-app.get("/api/auth/login", (req, res) => {
-  console.log("login route");
-});
-
-app.get("/api/auth/logout", (req, res) => {
-  console.log("login route");
-});
+app.use('/api/auth', authRoutes)
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
