778dda949a805930eb80fbc7d5b484e0c546a8da
Eliezer Nsengi <avaranstudios@gmail.com>
2024-08-16 08:29:08 +0200

    feat: add message routes

--- backend/server.js
+++ backend/server.js
@@ -12,6 +12,8 @@ const PORT = process.env.PORT || 5000;
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
+app.use("/api/message", authRoutes);
+
 
 app.get("/", (req, res) => {
   res.send("Hello from Nsengi!");
