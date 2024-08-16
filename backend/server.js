70a5e60987a37a6ee2d953582729b9d705a44889
Eliezer Nsengi <avaranstudios@gmail.com>
2024-08-16 08:33:03 +0200

    feat: add mesage routes

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,8 @@ import express from "express";
 import dotenv from "dotenv";
 
 import authRoutes from "./routes/auth.routes.js";
+import messageRoutes from "./routes/message.routes.js";
+
 import connectToMongoDb from "./db/connectToMongoDb.js";
 
 dotenv.config();
@@ -12,8 +14,7 @@ const PORT = process.env.PORT || 5000;
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
-app.use("/api/message", authRoutes);
-
+app.use("/api/message", messageRoutes);
 
 app.get("/", (req, res) => {
   res.send("Hello from Nsengi!");
