e611d3172f4b4aba4e6423c1f20465553b5ecee4
Eliezer Nsengi <avaranstudios@gmail.com>
2024-08-16 08:04:43 +0200

    feat: logout controller

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -28,8 +28,14 @@ export const login = async (req, res) => {
   }
 };
 
-export const logout = (req, res) => {
-  console.log("logout user");
+export const logout = async (req, res) => {
+  try {
+    res.cookie("jwt", "", { maxAge: 0 });
+    res.status(200).json({ message: "Logged out successfully!" });
+  } catch (error) {
+    console.log("Error in logout controller", error.message);
+    res.status(500).json({ error: error.message || "Internal server Error" });
+  }
 };
 
 export const signup = async (req, res) => {
