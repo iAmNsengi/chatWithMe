90713317d9fa81fec3aae0f57bd28ed01ce238ec
Eliezer Nsengi <avaranstudios@gmail.com>
2024-08-16 09:14:15 +0200

    feat: add conversation, send

--- backend/controllers/message.controller.js
+++ backend/controllers/message.controller.js
@@ -1,9 +1,34 @@
+import Conversation from "../models/conversation.model.js";
+import Message from "../models/message.model.js";
+
 export const sendMessage = async (req, res) => {
   try {
     const { message } = req.body;
-    const { id } = req.params;
+    const { id: receiverId } = req.params;
+    const senderId = req.user._id;
 
-    const senderId = "";
+    let conversation = await Conversation.findOne({
+      participants: { $all: [senderId, receiverId] },
+    });
+
+    if (!conversation) {
+      conversation = await Conversation.create({
+        participants: [senderId, receiverId],
+      });
+    }
+
+    const newMessage = new Message({
+      senderId,
+      receiverId,
+      message,
+    });
+    if (newMessage) {
+      conversation.messages.push(newMessage);
+    }
+
+    await conversation.save();
+    await newMessage.save();
+    res.status(201).json(newMessage);
   } catch (error) {
     console.log("Error in message controller", error.message);
     res.status(500).json({ error: error.message || "Internal server error" });
