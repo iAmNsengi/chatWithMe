ef0f3364274045ac6b87af62fd3157c419d06e68
Eliezer Nsengi <avaranstudios@gmail.com>
2024-08-16 08:51:39 +0200

    add: protect routes

--- backend/controllers/message.controller.js
+++ backend/controllers/message.controller.js
@@ -1,3 +1,11 @@
 export const sendMessage = async (req, res) => {
-  console.log(" Message sent");
+  try {
+    const { message } = req.body;
+    const { id } = req.params;
+
+    const senderId = "";
+  } catch (error) {
+    console.log("Error in message controller", error.message);
+    res.status(500).json({ error: error.message || "Internal server error" });
+  }
 };
--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,12 @@
 import express from "express";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 
 import authRoutes from "./routes/auth.routes.js";
 import messageRoutes from "./routes/message.routes.js";
 
 import connectToMongoDb from "./db/connectToMongoDb.js";
+import protectRoute from "./middleware/protectRoute.js";
 
 dotenv.config();
 
@@ -12,9 +14,9 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
-
+app.use(cookieParser());
 app.use("/api/auth", authRoutes);
-app.use("/api/message", messageRoutes);
+app.use("/api/messages", protectRoute, messageRoutes);
 
 app.get("/", (req, res) => {
   res.send("Hello from Nsengi!");
